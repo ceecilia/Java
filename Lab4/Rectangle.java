d85db9a442a8ba1defb0cbab19bae1e886d6acf4
ceecilia <ashicecilia0814@gmail.com>
2019-05-04 18:32:33 +0100

    lab3-6

---
+++ Lab4/Rectangle.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Rectangle here.
+ * 
+ * @author (Ashi Cecilia Matno U15/FNS/CSC/058) 
+ * @version (a version number or a date)
+ */
+class Rectangle {
+private double length , width;
+public Rectangle(double rectangleLength , double rectangleWidth) {
+length = rectangleLength;
+width = rectangleWidth;
+}
+public double area() {
+return length * width;
+}
+// Accessor methods
+public double getLength() {
+return length;
+}
+public double getWidth() {
+return width;
+}
+// Mutator methods
+public void setLength(double newLength) {
+length = newLength;
+}
+public void setRectangleWidth(float newWidth) {
+width = newWidth;
+}
+}
